c5cbc2d2acabbf4ec51a01a9ede593857516e46d
mukeshthedev <mukeshselvamani380@gmail.com>
2026-03-23 12:53:40 -0700

    use env variable for API URL

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:5000/api';
+const API_BASE = 'https://helpdesk-backend-cmvq.onrender.com';
 
 const api = axios.create({
   baseURL: API_BASE,
